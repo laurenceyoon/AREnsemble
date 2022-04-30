46e07159874ffa03de72f590f4027e34e87df807
YoungsterJohnny <yohanme@kaist.ac.kr>
2022-05-01 01:21:03 +0900

    Add actual music and instrument options(violin, piano, cello, viola)

--- Assets/Main/ARManager.cs
+++ Assets/Main/ARManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 namespace UnityEngine.XR.ARFoundation.Samples
 {
@@ -13,11 +14,17 @@ namespace UnityEngine.XR.ARFoundation.Samples
     /// and moved to the hit position.
     /// </summary>
     [RequireComponent(typeof(ARRaycastManager))]
+
     public class ARManager : MonoBehaviour
     {
         [SerializeField]
         [Tooltip("Instantiates this prefab on a plane at the touch location.")]
         GameObject m_PlacedPrefab;
+
+        public GameObject InstructionObject, menuObject;
+        public Text InstructionText;
+        private int counter=0;
+        private bool isDelay = false;
 
         /// <summary>
         /// The prefab to instantiate on touch.
@@ -35,11 +42,22 @@ namespace UnityEngine.XR.ARFoundation.Samples
         public GameObject spawnedObject { get; private set; }
 
         private List<FMOD.Studio.EventInstance> instances;
+        private List<string> names = new List<string>()
+        {
+            "Violin",
+            "Viola",
+            "Cello",
+            "Piano"
+        };
+        private List<GameObject> InstrumentPrefabs;
+
 
         void Awake()
         {
             m_RaycastManager = GetComponent<ARRaycastManager>();
             instances = new List<FMOD.Studio.EventInstance>();
+            InstrumentPrefabs = new List<GameObject>();
+            counter = 0;
         }
 
         bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -59,22 +77,61 @@ namespace UnityEngine.XR.ARFoundation.Samples
             if (!TryGetTouchPosition(out Vector2 touchPosition))
                 return;
             
-            if (m_RaycastManager.Raycast(touchPosition, s_Hits))
+            if (!isDelay && m_RaycastManager.Raycast(touchPosition, s_Hits))
             {
                 var hitPose = s_Hits[0].pose;
-                if (spawnedObject == null)
+                if (counter<4)
                 {
-                    spawnedObject=Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    isDelay = true;
+                    Invoke("setNextObject", 1.0f);
+                    spawnedObject =Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
                     spawnedObject.SetActive(true);
-                    var instance = FMODUnity.RuntimeManager.CreateInstance("event:/Test/AR-Instrument1");
+                    InstrumentPrefabs.Add(spawnedObject);
+                    var instance = FMODUnity.RuntimeManager.CreateInstance("event:/Test/AR-"+ names[counter]);
+                    //var instance = FMODUnity.RuntimeManager.CreateInstance("event:/Test/AR-Instrument1");
                     instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(spawnedObject));
                     instance.start();
+                    instance.setPaused(true);
                     instances.Add(instance);
+                    counter++;
                 }
+                /*
                 else
                 {
                     spawnedObject.transform.position=hitPose.position;
-                }
+                }*/
+            }
+        }
+
+        public void setNextObject() {
+            isDelay = false;
+            if (counter == 4)
+            {
+                showMenu();
+            }
+            else
+            {
+                InstructionText.text = "Place the " + names[counter];
+            }
+        }
+
+        public void showMenu() {
+            InstructionObject.SetActive(false);
+            menuObject.SetActive(true);
+        }
+
+        public void musicStart()
+        {
+            foreach (var instance in instances) {
+                instance.setPaused(false);
+            }
+        }
+
+        public void musicPause()
+        {
+            foreach (var instance in instances)
+            {
+                instance.setPaused(true);
             }
         }
 
