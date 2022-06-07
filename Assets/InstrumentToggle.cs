45d69feab813dc5801e0d73c1bdcaa572da86a15
YoungsterJohnny <yohanme@kaist.ac.kr>
2022-06-07 23:54:54 +0900

    [Feature] Show instrument names on button

--- Assets/InstrumentToggle.cs
+++ Assets/InstrumentToggle.cs
@@ -7,9 +7,25 @@ public class InstrumentToggle : MonoBehaviour
 {
     public int instrumentID;
     public Image image;
+    public Text InstrumentName;
     public void Init(int id)
     {
         instrumentID = id;
-        image.sprite = Resources.Load<Sprite>("UI/InstrumentSnapshots/"+instrumentID.ToString());   
+        //image.sprite = Resources.Load<Sprite>((instrumentID+1).ToString()+".png"); 
+        switch (instrumentID) {
+            case 0:
+                InstrumentName.text = "Violin";
+                break;
+            case 1:
+                InstrumentName.text = "Viola";
+                break;
+            case 2:
+                InstrumentName.text = "Cello";
+                break;
+            case 3:
+                InstrumentName.text = "Piano";
+                break;
+
+        }
     }
 }
